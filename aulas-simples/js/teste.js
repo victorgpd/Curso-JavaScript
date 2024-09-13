d95687202897a49efa7c01a75ea131e359a8a699
Victor <72409417+Victor007894@users.noreply.github.com>
2024-09-12 23:36:35 -0300

    teste

---
+++ aulas-simples/js/teste.js
@@ -0,0 +1,21 @@
+/*
+executa e depois verifica
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 6)
+
+c = 1
+
+verifica e depois executa
+while (c <= 6) {
+    console.log(c)
+    c++
+}
+
+repetição for
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+*/
